d0c9e52b2978963304b32c405990ed846b7655db
Adetunji Akinbami <adetunji@Adetunjis-MacBook-Pro.local>
2016-10-14 17:34:44 -0400

    Finish log in/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,13 @@
 Rails.application.routes.draw do
-    # get 'users/new'
-
-    get 'users/new'
-
+    get 'sessions/new'
+    #  get 'users/new'
     root 'static_pages#home'
-
     get 'help' => 'static_pages#help'
-
     get 'about' => 'static_pages#about'
-
     get 'contact' => 'static_pages#contact'
-
     get 'signup' => 'users#new'
-
+    get 'login' => 'sessions#new'
+    post 'login' => 'sessions#create'
+    delete 'logout' => 'sessions#destroy'
     resources :users
-
-    # root 'application#hello'
 end
